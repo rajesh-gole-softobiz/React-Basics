322681510570ae6504a373387c3010707ecd2147
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2023-03-28 18:34:44 +0530

    inline styling in JSX

--- l2-basics/src/App.tsx
+++ l2-basics/src/App.tsx
@@ -1,5 +1,11 @@
 import React, { Fragment } from 'react';
 
+const borderStyle = {
+  border : "1px solid",
+  backgroundColor: "black",
+  color: "white"
+}
+
 function App() {
   const users = [
     {
@@ -12,15 +18,30 @@ function App() {
     }
   ]
 
+  // const usersJSX = users.map((user)=>{
+  //   return (
+  //   <div id={`user-block-${user.id}`} key={user.id}>
+  //     <h1>{user.name}</h1>
+  //   </div>
+  // )})
   const usersJSX = users.map((user)=>{
     return (
-    <div id={`user-block-${user.id}`} key={user.id}>
-      <h1>{user.name}</h1>
-    </div>
+    <tr key={user.id}>
+      <td>{user.id}</td>
+      <td>{user.name}</td>
+    </tr>
   )})
   return (
     <Fragment>
-      { usersJSX }
+      <table style={borderStyle}>
+        <thead>
+            <th>Id</th>
+            <th>Name</th>
+        </thead>
+        <tbody>
+            { usersJSX }
+        </tbody>
+      </table>
     </Fragment>
   );
 }
