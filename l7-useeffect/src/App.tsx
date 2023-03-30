50b2fa74fc2c3e640e5ea82d236b90cd17de7a8c
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2023-03-30 15:12:34 +0530

    useEffect

--- l7-useeffect/src/App.tsx
+++ l7-useeffect/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0)
+  const [loading,setIsLoading] = useState(false)
+
+  useEffect(()=>{
+    console.log("useEffect");
+  },[count])
+
+  const handleIncrement = () =>{
+    setCount(count+1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {console.log("rendering")}
+
+    <h1>Count: {count}</h1>
+    <button onClick={handleIncrement}>Increment</button> &nbsp;
+    <button onClick={()=>{setIsLoading(!loading)}}>Loading</button>
+    </>
   );
 }
 
