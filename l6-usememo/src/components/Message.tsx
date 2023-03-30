ecf3d69f5c313a7fa365c1addee13a938ce7a6ed
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2023-03-30 13:14:11 +0530

    useMemo

--- l6-usememo/src/components/Message.tsx
+++ l6-usememo/src/components/Message.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 const Message = (props: propType) => {
     const {countNo} = props
+    // let number = 0;
+    // for(let i=0;i<500000000;i++){
+    //     number++;
+    // }
+    const res = useMemo(()=>{
     let number = 0;
     for(let i=0;i<500000000;i++){
         number++;
     }
+    return number
+    },[])
     console.log("Message rendered");
   return (
     <>
-    <h1>Number : {number}</h1>
+    <h1>Number : {res}</h1>
     <div>Send {countNo} message</div>
     </>
   )
